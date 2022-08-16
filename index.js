80a83b93e57fb48138ebbe88e0a8332809dcb6b4
Promise Okeke <plastamasta21@gmail.com>
2022-08-16 23:05:22 +0100

    made  the toggle text to change depending on the mode in

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const toggleButton = document.querySelector(".toggle-button");
 const toggleCase = document.querySelector(".toggle-case");
+const toggleText = document.querySelector(".toggle-text");
 const body = document.querySelector("body");
 let i = 0;
 toggleButton.addEventListener("click", () => {
@@ -7,9 +8,11 @@ toggleButton.addEventListener("click", () => {
   console.log(i);
   if (i % 2 === 0) {
     toggleCase.style.justifyContent = "flex-start";
+    toggleText.innerHTML = "Dark Mode";
     body.classList.remove("active");
   } else {
     toggleCase.style.justifyContent = "flex-end";
+    toggleText.innerHTML = "Light Mode";
     body.classList.add("active");
   }
 });
